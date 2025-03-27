8caa0fbd252e96fb90c6dcd198dcc013d1c25fd1
Drew Allred <drew.e.allred@gmail.com>
2025-03-27 13:49:02 -0400

    static landing page done

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,18 +1,29 @@
 
+import { useNavigate } from 'react-router-dom'
+import LandingButton from '../../assets/imports/landing-button.png'
+import LandingOutline from '../../assets/imports/landing-outline.png'
 import React from 'react'
 
 const Landing = () => {
     return (
         <section id="home">
             <div className="landing__discover">
-                <h4 className="landing__option">discover a.i.</h4>
+                <img src={LandingOutline} alt="" className="landing__discover--outline" />
+                <h4 className="landing__discover--option">
+                    <img src={LandingButton} alt="Discover" />
+                    discover a.i.
+                </h4>
             </div>
             <div className="landing__title">
                 <h1 className="landing__soph">Sophisticated</h1>
                 <h1 className="landing__skin">skincare</h1>
             </div>
             <div className="landing__test">
-                <h4 className="landing__option">take test</h4>
+                <img src={LandingOutline} alt="" className="landing__test--outline" />
+                <h4 className="landing__test--option">
+                    take test
+                    <img src={LandingButton} alt="Test" onClick={useNavigate('/introduction')} />
+                </h4>
             </div>
             <div className="landing__fine">
                 Skinstric developed an A.I. that creates
