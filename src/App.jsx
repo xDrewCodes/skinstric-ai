b4aac2aec94732a270c65611937e4a12372c1c83
Drew Allred <drew.e.allred@gmail.com>
2025-03-27 12:26:45 -0400

    nav, fonts, landing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path='/' exact element={ <Home /> } />
       </Routes>
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className="nav__title">
+                <h4 className="nav__logo">skinstric</h4>
+                <h4 className="nav__crumb">[ intro ]</h4>
+            </div>
+            <button className="nav__btn">enter code</button>
+        </nav>
+    )
+}
+
+export default Nav
