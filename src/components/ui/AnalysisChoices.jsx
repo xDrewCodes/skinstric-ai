75410db68d49742bc2918d83318e495efd28cd74
Drew Allred <drew.e.allred@gmail.com>
2025-03-28 13:28:30 -0400

    interactive demographic options

--- src/components/ui/AnalysisChoices.jsx
+++ src/components/ui/AnalysisChoices.jsx
@@ -1,22 +1,29 @@
 
 import React from 'react'
 
-const AnalysisChoices = () => {
+const AnalysisChoices = ({ setDemo, demo, demos }) => {
+
     return (
         <div className="analysis__info--buttons">
-                    <div className="analysis__info--button analysis__info--button--selected">
-                        <h5>Caucasian</h5>
-                        <h5>Race</h5>
-                    </div>
-                    <div className="analysis__info--button">
-                        <h5>20-29</h5>
-                        <h5>Age</h5>
-                    </div>
-                    <div className="analysis__info--button">
-                        <h5>Female</h5>
-                        <h5>Sex</h5>
-                    </div>
-                </div>
+            <div
+                onClick={() => setDemo('race')}
+                className={`analysis__info--button ${demo === 'race' && 'analysis__info--button--selected'}`}>
+                <h5>{demos.race}</h5>
+                <h5>Race</h5>
+            </div>
+            <div
+                onClick={() => setDemo('age')}
+                className={`analysis__info--button ${demo === 'age' && 'analysis__info--button--selected'}`}>
+                <h5>{demos.age}</h5>
+                <h5>Age</h5>
+            </div>
+            <div
+                onClick={() => setDemo('sex')}
+                className={`analysis__info--button ${demo === 'sex' && 'analysis__info--button--selected'}`}>
+                <h5>{demos.sex}</h5>
+                <h5>Sex</h5>
+            </div>
+        </div>
     )
 }
 
