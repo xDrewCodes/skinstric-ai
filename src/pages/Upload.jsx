0f1580d4a7357a8588b57ee456dce0a41f93ce45
Drew Allred <drew.e.allred@gmail.com>
2025-03-29 15:47:54 -0400

    trim upload file

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -4,11 +4,11 @@ import UploadOutline1 from '../assets/imports/outline.png'
 import UploadOutline2 from '../assets/imports/outline2.png'
 import UploadOutline3 from '../assets/imports/outline3.png'
 import CameraIcon from '../assets/imports/shutter.png'
-import GalleryIcon from '../assets/imports/gallery.png'
 import React from 'react'
 import ProceedButton from '../components/ui/ProceedButton'
 import axios from 'axios'
 import { useOutlineAnim } from '../anim'
+import UploadGallery from '../components/ui/UploadGallery'
 
 const Upload = ({ setDemos }) => {
 
@@ -51,39 +51,8 @@ const Upload = ({ setDemos }) => {
                     Allow A.I.<br />To Scan Your Face
                 </div>
             </div>
-            <div className="upload__gallery">
-                <div className="upload__gallery--outline">
-                    <img className="outline1" src={UploadOutline1} alt="" />
-                    <img className="outline2" src={UploadOutline2} alt="" />
-                    <img className="outline3" src={UploadOutline3} alt="" />
-                </div>
-                <img
-                    onClick={askFile}
-                    src={GalleryIcon}
-                    alt=""
-                    className="gallery__icon"
-                />
-                <input
-                    onChange={(e) => {
-                        const file = e.target.files[0]
-                        if (file) {
-                            const reader = new FileReader()
-                            reader.onloadend = function () {
-                                savePicture(reader.result)
-
-                            };
-                            reader.readAsDataURL(file)
-                        }
-                    }}
-                    type="file"
-                    accept="image/png, image/jpeg"
-                    id="gallery__input"
-                    style={{ display: 'none' }}
-                />
-                <div className="gallery__title">
-                    Allow A.I.<br />To Access Gallery
-                </div>
-            </div>
+            
+            <UploadGallery savePicture={savePicture} askFile={askFile} />
 
             <BackButton loc="/location" />
             <ProceedButton loc="/preparing" />
