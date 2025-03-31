e9868759d51c176f7c34494fbd61aba612f4ee7a
Drew Allred <drew.e.allred@gmail.com>
2025-03-31 11:18:38 -0400

    video/snapshot functionality

--- src/pages/Preparing.jsx
+++ src/pages/Preparing.jsx
@@ -3,7 +3,6 @@ import PrepOutline1 from '../assets/imports/outline.png'
 import PrepOutline2 from '../assets/imports/outline2.png'
 import PrepOutline3 from '../assets/imports/outline3.png'
 import React from 'react'
-import ProceedButton from '../components/ui/ProceedButton'
 
 const Preparing = () => {
     return (
@@ -15,8 +14,6 @@ const Preparing = () => {
                 Preparing Your Analysis...
 
             </section>
-
-            <ProceedButton loc="/analysis-menu" />
 
         </>
     )
--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,20 +1,69 @@
 import { useNavigate } from 'react-router-dom'
 import BackButton from '../components/ui/BackButton'
+import GoBackButton from '../assets/imports/button.png'
 import UploadOutline1 from '../assets/imports/outline.png'
 import UploadOutline2 from '../assets/imports/outline2.png'
 import UploadOutline3 from '../assets/imports/outline3.png'
 import CameraIcon from '../assets/imports/shutter.png'
 import GalleryIcon from '../assets/imports/gallery.png'
-import React from 'react'
+import VideoCamera from '../assets/imports/video-button.png'
+import VideoPointer from '../assets/imports/video-pointer.png'
+import React, { useState } from 'react'
 import ProceedButton from '../components/ui/ProceedButton'
 import axios from 'axios'
 
 const Upload = ({ setDemos }) => {
 
+    const [isRecording, setIsRecording] = useState(false)
     let navigate = useNavigate()
+
+    async function getCameraAccess() {
+        setIsRecording(true)
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true })
+
+            // Select the video element
+            const videoElement = document.querySelector('#camera__video')
+            if (videoElement) {
+                videoElement.srcObject = stream
+                videoElement.play()
+            } else {
+                console.error("Camera element not found.")
+            }
+        } catch (error) {
+            if (error.name === 'NotAllowedError') {
+                alert("Camera access was denied. Please allow access to use this feature.")
+            } else if (error.name === 'NotFoundError') {
+                alert("No camera device found. Please connect a camera and try again.")
+            } else {
+                console.error("Error accessing camera:", error)
+                alert("An unexpected error occurred while accessing the camera.")
+            }
+        }
+    }
 
     function askFile() {
         document.querySelector('#gallery__input').click()
+    }
+
+    function captureFrame() {
+        const videoElement = document.querySelector('#camera__video')
+        const canvas = document.querySelector('#capture__canvas')
+
+        if (videoElement && canvas) {
+            const context = canvas.getContext('2d')
+            canvas.width = videoElement.videoWidth
+            canvas.height = videoElement.videoHeight
+
+            // Draw the current frame of the video onto the canvas
+            context.drawImage(videoElement, 0, 0, canvas.width, canvas.height)
+
+            // Get the image as a base64 string
+            const imageData = canvas.toDataURL('image/png')
+            savePicture(imageData)
+        } else {
+            console.error("Video or canvas element not found.")
+        }
     }
 
     function savePicture(base64String) {
@@ -37,55 +86,100 @@ const Upload = ({ setDemos }) => {
 
     return (
         <section id="upload">
-            <div className="section-subhead">to start analysis</div>
-            <div className="upload__camera">
-                <div className="upload__camera--outline">
-                    <img src={UploadOutline1} alt="" />
-                    <img src={UploadOutline2} alt="" />
-                    <img src={UploadOutline3} alt="" />
-                </div>
-                <img src={CameraIcon} alt="" className="camera__icon" />
-                <div className="camera__title">
-                    Allow A.I.<br />To Scan Your Face
-                </div>
-            </div>
-            <div className="upload__gallery">
-                <div className="upload__gallery--outline">
-                    <img src={UploadOutline1} alt="" />
-                    <img src={UploadOutline2} alt="" />
-                    <img src={UploadOutline3} alt="" />
-                </div>
-                <img
-                    onClick={askFile}
-                    src={GalleryIcon}
-                    alt=""
-                    className="gallery__icon"
-                />
-                <input
-                    onChange={(e) => {
-                        const file = e.target.files[0]
-                        if (file) {
-                            const reader = new FileReader()
-                            reader.onloadend = function () {
-                                savePicture(reader.result)
+            {!isRecording
+                ?
+                <>
+                    <div className="section-subhead">to start analysis</div>
+                    <div className="upload__camera">
+                        <div className="upload__camera--outline">
+                            <img src={UploadOutline1} alt="" />
+                            <img src={UploadOutline2} alt="" />
+                            <img src={UploadOutline3} alt="" />
+                        </div>
+                        <img
+                            onClick={getCameraAccess}
+                            src={CameraIcon}
+                            alt=""
+                            className="camera__icon"
+                        />
+                        <div className="camera__title">
+                            Allow A.I.<br />To Scan Your Face
+                        </div>
+                    </div>
+                    <div className="upload__gallery">
+                        <div className="upload__gallery--outline">
+                            <img src={UploadOutline1} alt="" />
+                            <img src={UploadOutline2} alt="" />
+                            <img src={UploadOutline3} alt="" />
+                        </div>
+                        <img
+                            onClick={askFile}
+                            src={GalleryIcon}
+                            alt=""
+                            className="gallery__icon"
+                        />
+                        <input
+                            onChange={(e) => {
+                                const file = e.target.files[0]
+                                if (file) {
+                                    const reader = new FileReader()
+                                    reader.onloadend = function () {
+                                        savePicture(reader.result)
 
-                            };
-                            reader.readAsDataURL(file)
-                        }
-                    }}
-                    type="file"
-                    accept="image/png, image/jpeg"
-                    id="gallery__input"
-                    style={{ display: 'none' }}
-                />
-                <div className="gallery__title">
-                    Allow A.I.<br />To Access Gallery
+                                    };
+                                    reader.readAsDataURL(file)
+                                }
+                            }}
+                            type="file"
+                            accept="image/png, image/jpeg"
+                            id="gallery__input"
+                            style={{ display: 'none' }}
+                        />
+                        <div className="gallery__title">
+                            Allow A.I.<br />To Access Gallery
+                        </div>
+                    </div>
+
+                    <BackButton loc="/location" />
+                </>
+                :
+                <div className="upload__video">
+                    <div className='upload__video--nav'>
+                        <div className="upload__video--title nav__logo">Skinstric</div>
+                        <div className="upload__video--crumb">[ Upload ]</div>
+                    </div>
+                    <div className="upload__video--notif">Great Shot!</div>
+                    <div className="upload__video--button" onClick={captureFrame}>
+                        Take Picture
+                        <div className="upload__video--button--img">
+                            <img src={VideoCamera} alt="" />
+                        </div>
+                    </div>
+                    <div className="upload__video--info">
+                        <h4>To get a better result make sure to have</h4>
+                        <div className="upload__video--pointers">
+                            <div className="upload__video--pointer">
+                                <img src={VideoPointer} alt="" />
+                                neutral expression
+                            </div>
+                            <div className="upload__video--pointer">
+                                <img src={VideoPointer} alt="" />
+                                frontal pose
+                            </div>
+                            <div className="upload__video--pointer">
+                                <img src={VideoPointer} alt="" />
+                                adequate lighting
+                            </div>
+                        </div>
+                    </div>
+                    <img
+                    onClick={() => setIsRecording(false)}
+                    className="upload__video--back"
+                    src={GoBackButton} alt="" />
+                    <video id="camera__video" autoPlay muted></video>
+                    <canvas id="capture__canvas" style={{ display: 'none' }}></canvas>
                 </div>
-            </div>
-
-            <BackButton loc="/location" />
-            <ProceedButton loc="/preparing" />
-
+            }
         </section>
     )
 }
