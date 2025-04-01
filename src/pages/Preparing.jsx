504e7c14d452b4c9c035b23c9ae74d4b4f2da3dc
Drew Allred <drew.e.allred@gmail.com>
2025-04-01 05:49:47 -0400

    merge fixes

--- src/pages/Preparing.jsx
+++ src/pages/Preparing.jsx
@@ -4,8 +4,6 @@ import PrepOutline1 from '../assets/imports/outline.png'
 import PrepOutline2 from '../assets/imports/outline2.png'
 import PrepOutline3 from '../assets/imports/outline3.png'
 import React from 'react'
-import ProceedButton from '../components/ui/ProceedButton'
-import { useOutlineAnim } from '../anim'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 
