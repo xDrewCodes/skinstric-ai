5949873b87bd0f84c3388d768e7f7acd51a46f75
Drew Allred <drew.e.allred@gmail.com>
2025-04-22 11:35:31 -0400

    vercel deploy api fixes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 const express = require('express')
 const path = require('path')
-const dbu = require('./dbUtilsjs.js')
+const dbu = require('./server/dbUtilsjs.js')
 
 const app = express()
 const port = process.env.PORT || 8000
 
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, './build')))
 
 app.get('/api/users', async ( req, res ) => {
 
